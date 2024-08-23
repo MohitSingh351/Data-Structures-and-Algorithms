bb3961dc8cc381200ff516263ec5e5d5570bdf98
Mohit Singh <mohitrsingh2005@gmail.com>
2024-08-24 00:50:09 +0530

    Finding duplicates complete

--- Arrays/findingDuplicates.cpp
+++ Arrays/findingDuplicates.cpp
@@ -9,7 +9,7 @@ using namespace std;
 
 // Function to find duplicates in an array
 // Time complexity: O(n)
-int findDuplicatesM1(int A[], int length){
+void findDuplicatesM1(int A[], int length){
     int lastDuplicate = 0;
     for (int i = 0; i < length-1; i++) {
         if (A[i] == A[i+1] && A[i] != lastDuplicate) {
@@ -19,9 +19,9 @@ int findDuplicatesM1(int A[], int length){
     }
 }
 
-// Function to dind duplicates in an array and also to count number of duplicates
+// Function to find duplicates in an array and also to count number of duplicates
 // Time complexity: O(n)
-int findDuplicatesM2(int A[], int length) {
+void findDuplicatesM2(int A[], int length) {
     int i, j;
     for (i=0; i<length-1; i++) {
         if (A[i] == A[i+1]) {
@@ -47,7 +47,7 @@ int maxElement(int A[], int length) {
 
 // Function to find duplicates in an array using hashing
 // Time complexity: O(n)
-int findDuplicatesM3(int A[], int length) {
+void findDuplicatesM3(int A[], int length) {
     int max = maxElement(A, length);
     int *H = new int[max + 1];
     int h_length = max + 1;
@@ -62,6 +62,45 @@ int findDuplicatesM3(int A[], int length) {
             cout << "Duplicate element is: " << i << " and it appears " << H[i] << " times." << endl;
         }
     }
+    delete[] H;
+}
+
+void findDuplicatesInUnsortedArrayM1(int A[], int length) {
+    for(int i=0; i<length-1; i++) {
+        int count = 1;
+        if(A[i] != -1) {
+            for(int j=i+1; j<length; j++) {
+                if(A[i] == A[j]) {
+                    count++;
+                    A[j] = -1;
+                }
+            }
+            if(count > 1) {
+                cout << "Duplicate element is: " << A[i] << " and it appears " << count << " times." << endl;
+            }
+        }
+    }
+}
+
+// Find duplicates in an unsorted array using hashing
+void findDuplicatesUsingHashing(int A[], int length) {
+    int max = maxElement(A, length);
+    int w_length = max + 1;
+    int *W = new int[w_length];
+    for(int i=0; i<w_length; i++) {
+        W[i] = 0;
+    }
+    for(int i=0; i<length; i++) {
+        W[A[i]]++;
+    }
+    
+    for(int i=0; i<w_length; i++) {
+        if(W[i] > 1) {
+            cout << "Duplicate element is: " << i << " and it appears " << W[i] << " times." << endl;
+        }
+    }
+    
+    delete[] W;
 }
 
 int main() {
@@ -73,5 +112,13 @@ int main() {
     findDuplicatesM2(A, 10);  // Duplicates should be 8 and 15
     cout << endl;
     findDuplicatesM3(A, 10);  // Duplicates should be 8 and 15
+    cout << endl;
+    cout << "Unsorted array" << endl << "------------------";
+    cout << endl;
+    int B[] = {8, 3, 6, 4, 6, 5, 6, 8, 2, 7};
+    findDuplicatesInUnsortedArrayM1(B, 10);  // Duplicates should be 8, 6
+    cout << endl;
+    int C[] = {8, 3, 6, 4, 6, 5, 6, 8, 2, 7};
+    findDuplicatesUsingHashing(C, 10);  // Duplicates should be 8, 6
     return 0;
 }
