b96941f4745d1a4a6c3953d10b9d14906408aade
Mohit Singh <mohitrsingh2005@gmail.com>
2024-08-23 16:27:16 +0530

    Complete finding duplicates in sorted array using 3 methods

---
+++ Arrays/findingDuplicates.cpp
@@ -0,0 +1,77 @@
+// Finding duplicates in an array
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+// Function to find duplicates in an array
+// Time complexity: O(n)
+int findDuplicatesM1(int A[], int length){
+    int lastDuplicate = 0;
+    for (int i = 0; i < length-1; i++) {
+        if (A[i] == A[i+1] && A[i] != lastDuplicate) {
+            cout << "Duplicate element is: " << A[i] << endl;
+            lastDuplicate = A[i];
+        }
+    }
+}
+
+// Function to dind duplicates in an array and also to count number of duplicates
+// Time complexity: O(n)
+int findDuplicatesM2(int A[], int length) {
+    int i, j;
+    for (i=0; i<length-1; i++) {
+        if (A[i] == A[i+1]) {
+            j = i+1;
+            while(A[j] == A[i]){
+                j++;
+            }
+            cout << "Duplicate element is: " << A[i] << " and it appears " << j-i << " times." << endl;
+            i = j-1;
+        }
+    }
+}
+
+int maxElement(int A[], int length) {
+    int max = A[0];
+    for (int i = 1; i < length; i++) {
+        if (A[i] > max) {
+            max = A[i];
+        }
+    }
+    return max;
+}
+
+// Function to find duplicates in an array using hashing
+// Time complexity: O(n)
+int findDuplicatesM3(int A[], int length) {
+    int max = maxElement(A, length);
+    int *H = new int[max + 1];
+    int h_length = max + 1;
+    for(int i=0; i<h_length; i++) {
+        H[i] = 0;
+    }
+    for(int i=0; i<length; i++) {
+        H[A[i]]++;
+    }
+    for(int i=0; i<h_length; i++) {
+        if (H[i] > 1) {
+            cout << "Duplicate element is: " << i << " and it appears " << H[i] << " times." << endl;
+        }
+    }
+}
+
+int main() {
+
+    // Test case 1
+    int A[] = {3, 6, 8, 8, 10, 12, 15, 15, 15, 20};
+    findDuplicatesM1(A, 10);  // Duplicates should be 8 and 15
+    cout << endl;
+    findDuplicatesM2(A, 10);  // Duplicates should be 8 and 15
+    cout << endl;
+    findDuplicatesM3(A, 10);  // Duplicates should be 8 and 15
+    return 0;
+}
